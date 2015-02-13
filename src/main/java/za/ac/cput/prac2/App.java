4ed8ca26ec4b8c5e43b2b6d3ccead555a8c43e5d
Ephraim <ehatever@put.ac.za>
2015-02-13 13:11:33 +0200

    initial commit

--- src/main/java/za/ac/cput/prac2/App.java
+++ src/main/java/za/ac/cput/prac2/App.java
@@ -6,7 +6,45 @@ package za.ac.cput.prac2;
  */
 public class App 
 {
-    public float testInt(){
-        return 13;
+    public float getFloat(){
+
+        float sum = 125 * 1;
+        return sum;
+    }
+
+    public int getInt(){
+
+        return 12;
+    }
+
+    public String getObj(){
+        return "thabo";
+    }
+
+    public boolean getTruth(){
+        return true;
+    }
+
+    public boolean getFalse(){
+        return false;
+    }
+
+    public String getNull(){
+        String name = null;
+        return name;
+    }
+
+    public String getNotNull(){
+        String name = "thabo";
+        return name;
+    }
+
+    public int [] getArray(){
+        int [] myAarray = {12,12,25,45,2};
+        return myAarray;
+    }
+
+    public static double getTimeOut(int a,int b){
+        return a*b;
     }
 }
--- src/test/java/za/ac/cput/prac2/Apptest.java
+++ src/test/java/za/ac/cput/prac2/Apptest.java
@@ -2,16 +2,83 @@ package za.ac.cput.prac2;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.*;
 
-
-/**
- * Created by student on 2015/02/10.
- */
 public class Apptest extends TestCase {
+    App myApp = new App();
+    App myApp2 = myApp;
+
+    @BeforeClass
+    public static void beforeClass() {
+
+        System.out.println("In before class");
+    }
+
+    @Test
+    public void testFloat() throws Exception {
+
+        Assert.assertTrue(myApp.getFloat() == 125.0);
+    }
+
+    @Test
+    public void testInt() throws Exception{
+        Assert.assertEquals(myApp.getInt(), 12);
+    }
 
 
-    public void testInt() throws Exception {
-        App myApp = new App();
-        assertEquals("13",myApp.testInt());
+    @Test
+    public void testObj() throws Exception{
+        Assert.assertSame(myApp.getObj(), "thabo");
+    }
+
+    @Test
+    public void testObjId() throws Exception{
+        Assert.assertSame(myApp, myApp2);
+    }
+
+     @Test
+     public void testTrue() throws Exception{
+         Assert.assertEquals(myApp.getTruth(), true);
+     }
+
+    @Test
+    public void testFalse() throws Exception{
+        Assert.assertEquals(myApp.getFalse(), false);
+    }
+
+    @Test
+    public void testNull() throws Exception{
+        Assert.assertNull(myApp.getNull(), null);
+    }
+
+    @Test
+    public void testNotNull() throws Exception {
+        Assert.assertNotNull(myApp.getNull(), "thabo");
+    }
+
+     @Test
+     public void testFail() throws Exception {
+         //Assert.fail("fail test");
+     }
+
+    @Test
+    public void testArray() throws Exception{
+        int [] myArray2 = {1,2,3,4,5};
+
+       // Assert.assertArrayEquals(myApp.getArray(),myArray2);
+    }
+
+    @Test (timeout = 9000)
+    public void timeTest()throws Exception{
+        double ans = myApp.getTimeOut(12,2);
+    }
+
+    @AfterClass
+    public static void afterClass() {
+
+        System.out.println("In after class");
     }
 }
